3aad10152f15cdc2ef68efc41b1d7a387d28b7ef
jackzhou2007 <jackzhou2007@outlook.com>
2018-05-31 18:11:46 +0800

    NCALive V1.0

---
+++ NCALive2018V01/NCALive2018V01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCALive2018V01
+//
+//  Created by jackzhou2007 on 2018/5/31.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
